9914e4c5747bde4292b4adb1478ea4caaaa7b520
samisihvonen <sami.sihvonen1984@gmail.com>
2026-01-30 10:39:46 +0200

    feat: Add health endpoint

---
+++ backend/src/test/java/com/example/backend/controller/HealthControllerTest.java
@@ -0,0 +1,12 @@
+```java
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HealthController.class)
+public class HealthControllerTest {
+    
+}
+```
